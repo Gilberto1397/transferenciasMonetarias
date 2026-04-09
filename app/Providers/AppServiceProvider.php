bcc4a5bcfa92ba9d9e89da194e1466e8d5a72cd6
Gilberto AJ <gilberto-junior@outlook.com>
2026-04-09 19:41:54 -0300

    Adicionando bindings para repositórios de contas no AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,21 @@
 
 namespace App\Providers;
 
+use app\Contracts\FisicAccountRepository;
+use app\Contracts\JuristicAccountRepository;
+use app\Contracts\UserRepository;
+use app\Repositories\JuristicAccountRepositoryEloquent;
+use app\Repositories\UserRepositoryEloquent;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public array $bindings = [
+        UserRepository::class => UserRepositoryEloquent::class,
+        JuristicAccountRepository::class => JuristicAccountRepositoryEloquent::class,
+        FisicAccountRepository::class => JuristicAccountRepositoryEloquent::class
+    ];
+
     /**
      * Register any application services.
      *
