ff630cb17ad9471249a4fa6830bb3bd1394e9a29
Gilberto AJ <gilberto-junior@outlook.com>
2026-04-10 22:15:36 -0300

    Ajustando arquivo de rotas de API

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::prefix('v1')->group(function () {
     Route::prefix('contas')->group(function () {
         Route::post('', [AccountController::class, 'createAccount']);
