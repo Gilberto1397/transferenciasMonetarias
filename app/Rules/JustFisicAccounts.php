9be3b02be6514852c7d19f1cbe256fb7f33a515d
Gilberto AJ <gilberto-junior@outlook.com>
2026-04-14 07:41:29 -0300

    Adicionando regra de validação para contas físicas

---
+++ app/Rules/JustFisicAccounts.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\User;
+use Illuminate\Contracts\Validation\Rule;
+
+class JustFisicAccounts implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $user = User::fromQuery('
+            SELECT 1 FROM users
+                     INNER JOIN juristicaccounts ON users.id = juristicaccounts.juristicaccount_user
+                     WHERE id = :userId
+        ', ['userId' => $value]);
+        return $user->count() === 0;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'A conta de origem deve ser do tipo física!';
+    }
+}
