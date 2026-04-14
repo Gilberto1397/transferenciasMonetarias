67ae1aa9bee5d05bb324badfb33452285e57cdaf
Gilberto AJ <gilberto-junior@outlook.com>
2026-04-13 21:03:49 -0300

    Adicionando cliente de notificação para gerenciar transferências

---
+++ app/Clients/NotificationClient.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Clients;
+
+use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
+
+class NotificationClient
+{
+    const BASE_URL = 'https://util.devi.tools/api/v1/notify';
+    private Client $client;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+    }
+
+    public function throwNotification(): bool
+    {
+        /**
+         * @var ResponseInterface $response
+         */
+        $response = $this->client->postAsync(self::BASE_URL, ['http_errors' => false])->wait();
+
+        /**
+         * @var object{status: string, data: object{authorization: bool}} $responseObject
+         */
+        $responseObject = json_decode($response->getBody()->getContents());
+
+        if ($response->getStatusCode() !== 204 && $response->getStatusCode() !== 504) {
+            throw new \DomainException('Falha ao notificar transferência! Tente novamente.');
+        }
+        if ($response->getStatusCode() === 504 && !empty($responseObject->status) && $responseObject->status === 'error') {
+            return false;
+        }
+        if ($response->getStatusCode() === 204 && is_null($responseObject)) {
+            return true;
+        }
+        throw new \DomainException('Falha no serviço de notificação! Tente novamente.');
+    }
+}
