40410720da83ab3572de25a26fa044fbf7241e24
Gilberto AJ <gilberto-junior@outlook.com>
2026-04-24 08:43:00 -0300

    Ajustando testes do TransferController para utilizar QueueFake e verificar conteúdo de resposta apenas se não estiver vazio

--- tests/Feature/Controllers/TransferControllerTest.php
+++ tests/Feature/Controllers/TransferControllerTest.php
@@ -5,15 +5,17 @@ namespace Tests\Feature\Controllers;
 use App\Jobs\NotifyUserJob;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Queue;
+use Illuminate\Support\Testing\Fakes\QueueFake;
 use Tests\TestCase;
 
 class TransferControllerTest extends TestCase
 {
+    private QueueFake $queueFake;
     protected function setUp(): void
     {
         parent::setUp();
         DB::beginTransaction();
-        Queue::fake();
+        $this->queueFake = Queue::fake();
     }
 
     protected function tearDown(): void
@@ -37,59 +39,68 @@ class TransferControllerTest extends TestCase
          * When - Act
          */
         $response = $this->json('PUT', '/api/v1/transfer', $payload);
-        $objetoResposta = json_decode($response->getContent());
 
-        /**
-         * Then - Assert
-         */
-        $this->assertObjectHasProperty('message', $objetoResposta, 'A resposta deve conter o atributo "message".');
-        $this->assertObjectHasProperty('error', $objetoResposta, 'A resposta deve conter o atributo "error".');
+        if (!empty($response->getContent())) {
 
-        if ($response->getStatusCode() === 200) {
-            $this->assertSame(200, $response->getStatusCode(), 'A resposta deve ter o status 200 OK.');
-            $this->assertFalse($objetoResposta->error, "A propriedade 'error' deve ser falsa.");
-            $this->assertSame(
-                $objetoResposta->message,
-                'Transferência realizada com sucesso!',
-                "A mensagem de sucesso deve ser 'Transferência realizada com sucesso!'"
-            );
-            $this->assertTrue(Queue::hasPushed(NotifyUserJob::class), 'O job NotifyUserJob não foi despachado.');
-            $this->assertCount(1, Queue::pushed(NotifyUserJob::class), 'O job NotifyUserJob foi despachado mais de uma vez.');
-        } else {
-            $this->assertSame(403, $response->getStatusCode(), 'A resposta deve ter o status 403.');
-            $this->assertTrue($objetoResposta->error, "A propriedade 'error' deve ser falsa.");
-            $this->assertSame(
-                $objetoResposta->message,
-                'Transferência não autorizada!',
-                "A mensagem de sucesso deve ser 'Transferência não autorizada!'"
-            );
+            /**
+             * @var \StdClass $objetoResposta
+             */
+            $objetoResposta = json_decode($response->getContent());
+
+            /**
+             * Then - Assert
+             */
+            $this->assertObjectHasProperty('message', $objetoResposta, 'A resposta deve conter o atributo "message".');
+            $this->assertObjectHasProperty('error', $objetoResposta, 'A resposta deve conter o atributo "error".');
+
+            if ($response->getStatusCode() === 200) {
+                $this->assertSame(200, $response->getStatusCode(), 'A resposta deve ter o status 200 OK.');
+                $this->assertFalse($objetoResposta->error, "A propriedade 'error' deve ser falsa.");
+                $this->assertSame(
+                    $objetoResposta->message,
+                    'Transferência realizada com sucesso!',
+                    "A mensagem de sucesso deve ser 'Transferência realizada com sucesso!'"
+                );
+                $this->assertTrue($this->queueFake->hasPushed(NotifyUserJob::class), 'O job NotifyUserJob não foi despachado.');
+                $this->assertCount(1, $this->queueFake->pushed(NotifyUserJob::class), 'O job NotifyUserJob foi despachado mais de uma vez.');
+            } else {
+                $this->assertSame(403, $response->getStatusCode(), 'A resposta deve ter o status 403.');
+                $this->assertTrue($objetoResposta->error, "A propriedade 'error' deve ser falsa.");
+                $this->assertSame(
+                    $objetoResposta->message,
+                    'Transferência não autorizada!',
+                    "A mensagem de sucesso deve ser 'Transferência não autorizada!'"
+                );
+            }
         }
     }
 
     /**
      * @dataProvider invalidPayloadProvider
+     * @param array<string, int|string> $payload
      */
     public function testCreateAccountValidationErrorWhenPayloadIsInvalid(array $payload): void
     {
         /**
-         * Given - Arrange
-         */
-        $payload = [];
-
-        /**
          * When - Act
          */
         $response = $this->json('PUT', '/api/v1/transfer', $payload);
-        $objetoResposta = json_decode($response->getContent());
 
-        /**
-         * Then - Assert
-         */
-        $this->assertSame(406, $response->getStatusCode(), 'A resposta deve ter o status 406.');
-        $this->assertObjectHasProperty('messages', $objetoResposta, 'A resposta deve conter o atributo "messages".');
-        $this->assertObjectHasProperty('error', $objetoResposta, 'A resposta deve conter o atributo "error".');
+        if (!empty($response->getContent())) {
+            /**
+             * @var \StdClass $objetoResposta
+             */
+            $objetoResposta = json_decode($response->getContent());
 
-        $this->assertTrue($objetoResposta->error, "A propriedade 'error' deve ser verdadeira.");
+            /**
+             * Then - Assert
+             */
+            $this->assertSame(406, $response->getStatusCode(), 'A resposta deve ter o status 406.');
+            $this->assertObjectHasProperty('messages', $objetoResposta, 'A resposta deve conter o atributo "messages".');
+            $this->assertObjectHasProperty('error', $objetoResposta, 'A resposta deve conter o atributo "error".');
+
+            $this->assertTrue($objetoResposta->error, "A propriedade 'error' deve ser verdadeira.");
+        }
     }
 
     /**
